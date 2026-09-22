dd63a364c7a86a0eee6e221c54cd2f00105e13c5
KoreNat <NGaneshina@gmail.com>
2026-09-22 08:27:46 +0200

    Add ClipSong Android project

---
+++ clipsong/app/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.clipsong"
+    compileSdk = 37
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    defaultConfig {
+        applicationId = "com.example.clipsong"
+        minSdk = 29
+        targetSdk = 37
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
