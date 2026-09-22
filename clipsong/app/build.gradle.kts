00ee907f02f03c285658c837579bcec752429425
KoreNat <NGaneshina@gmail.com>
2026-09-22 09:14:04 +0200

    Bump ClipSong to 0.3

--- clipsong/app/build.gradle.kts
+++ clipsong/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.example.clipsong"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
     }
 }
