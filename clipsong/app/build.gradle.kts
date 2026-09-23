c994552bebc326bf3116a97656e7140e45ffffeb
KoreNat <NGaneshina@gmail.com>
2026-09-23 22:03:42 +0200

    Bump ClipSong to 0.4.1

--- clipsong/app/build.gradle.kts
+++ clipsong/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.example.clipsong"
         minSdk = 29
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.4.0"
+        versionCode = 6
+        versionName = "0.4.1"
     }
 
     buildTypes {
